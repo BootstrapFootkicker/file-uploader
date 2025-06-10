3e30cf149376c845c7e904fb34b75a0fd907031d
BootstrapFootkicker <ksealy93@gmail.com>
2025-06-10 13:23:20 -0400

    drpdown work

--- public/javascripts/folderScripts.js
+++ public/javascripts/folderScripts.js
@@ -1,5 +1,23 @@
 //todo work in here first 3/2/25
 
+document.addEventListener('click',e => {
+    const isDropdownButton = e.target.matches('[data-dropdown-button]');
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return;
+
+    if(isDropdownButton){
+        currentDropdown = e.target.closest('[data-dropdown]');
+        currentDropdown.classList.toggle('active');
+    }
+
+    document.querySelectorAll('[data-dropdown].active').forEach(dropdown => {
+        if(dropdown === currentDropdown) return;
+        dropdown.classList.remove('active');
+    }
+    );
+})
+
+
+
 function addFolderForm(formFunction) {
     showPopup(formFunction);
 }
@@ -134,3 +152,4 @@ async function fetchUserFolders() {
         alert("An error occurred while fetching folders.");
     }
 }
+
